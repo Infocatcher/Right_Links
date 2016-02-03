c541d26536034760928082bb6ce46ec0c6455a85
Infocatcher <infocatcher.dev.null@gmail.com>
2016-02-03 14:34:39 +0300

    Reduce size of frame script to improve memory usage (#21)

--- chrome/content/content.js
+++ chrome/content/content.js
@@ -1,8 +1,7 @@
-Components.utils.import("resource://gre/modules/Services.jsm");
-this.__defineGetter__("detect", function() {
-	delete this.detect;
-	Components.utils.import("chrome://rightlinks/content/detect.jsm");
-	return detect;
+this.__defineGetter__("contentUtils", function() {
+	delete this.contentUtils;
+	Components.utils.import("chrome://rightlinks/content/contentUtils.jsm");
+	return contentUtils;
 });
 
 var remoteFrameHandler = {
@@ -25,7 +24,7 @@ var remoteFrameHandler = {
 			case "mousedown":
 			case "mouseup":
 			case "click":
-				this.handleMouseEvent(e);
+				contentUtils.handleMouseEvent(sendSyncMessage, e);
 			break;
 			case "unload":
 				if(content && content.location != "about:blank")
@@ -41,74 +40,8 @@ var remoteFrameHandler = {
 					this.destroy();
 			break;
 			case "DispatchMouseEvents":
-				this.dispatchMouseEvents(msg.data.types, msg.data.options);
+				contentUtils.dispatchMouseEvents(msg.data.types, msg.data.options);
 		}
-	},
-	handleMouseEvent: function(e) {
-		var it = detect.getItem(e);
-		var h = it && detect.getHref(it, e);
-		if(!h)
-			return;
-		var trg = e.originalTarget;
-		var clonedEvent = {
-			originalTarget: {
-				localName: trg.localName,
-				ownerDocument: {
-					documentURI: trg.ownerDocument.documentURI
-				}
-			},
-			view: {
-				top: null
-			},
-			_rightLinksItem: {
-				localName: it.localName,
-				ownerDocument: {
-					documentURI: it.ownerDocument.documentURI,
-					location: { href: it.ownerDocument.documentURI },
-					nodePrincipal: it.ownerDocument.nodePrincipal //~ todo: test
-				},
-				_rightLinksURL: h
-			},
-			_rightLinksStop: false,
-			_rightLinksURL: h,
-			_rightLinksType: detect.itemType
-		};
-		for(var p in e) {
-			var v = e[p];
-			if(detect.isPrimitive(v))
-				clonedEvent[p] = v;
-		}
-		var ret = sendSyncMessage("RightLinks:Event", clonedEvent);
-		if(ret && ret[0]) {
-			e.preventDefault();
-			e.stopPropagation();
-			"stopImmediatePropagation" in e && e.stopImmediatePropagation();
-		}
-	},
-	dispatchMouseEvents: function(evtTypes, opts) {
-		var item = detect.origItem;
-		var origEvt = detect.event;
-		var doc = item.ownerDocument || item.document || item;
-		var win = doc.defaultView;
-		evtTypes.forEach(function(evtType) {
-			var evt = new win.MouseEvent(evtType, {
-				bubbles: true,
-				cancelable: true,
-				view: win,
-				detail: 1,
-				screenX: origEvt.screenX,
-				screenY: origEvt.screenY,
-				clientX: origEvt.clientX,
-				clientY: origEvt.clientY,
-				ctrlKey:  opts.ctrlKey  || false,
-				altKey:   opts.altKey   || false,
-				shiftKey: opts.shiftKey || false,
-				metaKey:  opts.metaKey  || false,
-				button:   opts.button   || 0,
-				relatedTarget: null
-			});
-			item.dispatchEvent(evt);
-		});
 	}
 };
 remoteFrameHandler.init(true);
---
+++ chrome/content/contentUtils.jsm
@@ -0,0 +1,76 @@
+var EXPORTED_SYMBOLS = ["contentUtils"];
+
+this.__defineGetter__("detect", function() {
+	delete this.detect;
+	Components.utils.import("chrome://rightlinks/content/detect.jsm");
+	return detect;
+});
+
+var contentUtils = {
+	handleMouseEvent: function(sendSyncMessage, e) {
+		var it = detect.getItem(e);
+		var h = it && detect.getHref(it, e);
+		if(!h)
+			return;
+		var trg = e.originalTarget;
+		var clonedEvent = {
+			originalTarget: {
+				localName: trg.localName,
+				ownerDocument: {
+					documentURI: trg.ownerDocument.documentURI
+				}
+			},
+			view: {
+				top: null
+			},
+			_rightLinksItem: {
+				localName: it.localName,
+				ownerDocument: {
+					documentURI: it.ownerDocument.documentURI,
+					location: { href: it.ownerDocument.documentURI },
+					nodePrincipal: it.ownerDocument.nodePrincipal //~ todo: test
+				},
+				_rightLinksURL: h
+			},
+			_rightLinksStop: false,
+			_rightLinksURL: h,
+			_rightLinksType: detect.itemType
+		};
+		for(var p in e) {
+			var v = e[p];
+			if(detect.isPrimitive(v))
+				clonedEvent[p] = v;
+		}
+		var ret = sendSyncMessage("RightLinks:Event", clonedEvent);
+		if(ret && ret[0]) {
+			e.preventDefault();
+			e.stopPropagation();
+			"stopImmediatePropagation" in e && e.stopImmediatePropagation();
+		}
+	},
+	dispatchMouseEvents: function(evtTypes, opts) {
+		var item = detect.origItem;
+		var origEvt = detect.event;
+		var doc = item.ownerDocument || item.document || item;
+		var win = doc.defaultView;
+		evtTypes.forEach(function(evtType) {
+			var evt = new win.MouseEvent(evtType, {
+				bubbles: true,
+				cancelable: true,
+				view: win,
+				detail: 1,
+				screenX: origEvt.screenX,
+				screenY: origEvt.screenY,
+				clientX: origEvt.clientX,
+				clientY: origEvt.clientY,
+				ctrlKey:  opts.ctrlKey  || false,
+				altKey:   opts.altKey   || false,
+				shiftKey: opts.shiftKey || false,
+				metaKey:  opts.metaKey  || false,
+				button:   opts.button   || 0,
+				relatedTarget: null
+			});
+			item.dispatchEvent(evt);
+		});
+	}
+};

Infocatcher/Right_Links

commit 65e39809727d285bb884c6c6204cdfdecfc99a5e

Correctly dispatch fake mouseup event in e10s mode

chrome/content/contentUtils.jsm
@@ -102,6 +102,7 @@ var contentUtils = {
 		var origEvt = detect.event;
 		var doc = item.ownerDocument || item.document || item;
 		var win = doc.defaultView;
+		var trg = opts.isFake ? win : item;
 		evtTypes.forEach(function(evtType) {
 			var evt = new win.MouseEvent(evtType, {
 				bubbles: true,
@@ -120,7 +121,7 @@ var contentUtils = {
 				relatedTarget: null
 			});
 			evt._rightLinksIgnore = true;
-			item.dispatchEvent(evt);
+			trg.dispatchEvent(evt);
 		});
 	},
 	loadURI: function(uri) {

chrome/content/rightlinks.js
@@ -734,9 +734,11 @@ var rightLinks = {
 				if(this.pu.pref("fakeMouseup")) {
 					var fakeTarget = !this.isChromeWin(e.view)
 						&& this.pu.pref("fakeMouseup.content")
-						? e.view.document || e.view
+						? "_rightLinksItem" in e
+							? { _rightLinksIsFake: true }
+							: e.view.document || e.view
 						: gBrowser.selectedBrowser.parentNode;
-					this.createMouseEvents(e, fakeTarget, ["mouseup"], 0)();
+					this.createMouseEvents(e, fakeTarget, ["mouseup"], { button: 0, isFake: true })();
 				}
 				if(this.isChromeWin(e.view.top))
 					this.stopEvent(e);
@@ -1281,7 +1283,12 @@ var rightLinks = {
 	createMouseEvents: function(origEvt, item, evtTypes, opts) {
 		if(typeof opts == "number")
 			opts = { button: opts };
-		if(this.isMultiProcess && this.event && "_rightLinksItem" in this.event)
+		if(
+			this.isMultiProcess
+			&& this.event
+			&& "_rightLinksItem" in this.event
+			&& "_rightLinksIsFake" in item
+		)
 			return this.createRemoteMouseEvents(evtTypes, opts);
 		var evts = evtTypes.map(function(evtType) {
 			return this.createMouseEvent(origEvt, item, evtType, opts);

commit 3a4ee81f362a897140d388814610b10add86fd26

Fix links with click handlers (regression in 65e39809727d285bb884c6c6204cdfdecfc99a5e commit) voidURI || dummyURI -> createMouseEvents -> "_rightLinksIsFake" in item

chrome/content/contentUtils.jsm
@@ -48,6 +48,7 @@ var contentUtils = {
 					},
 					nodePrincipal: itDoc.nodePrincipal //~ todo: test
 				},
+				_rightLinksIsFake: true,
 				_rightLinksURL: h,
 				_rightLinksIsDummy: isDummy,
 				_rightLinksIsCanvas: it instanceof itDoc.defaultView.HTMLCanvasElement,